3bf02b139898d13f14d89cd3b76666694a51e9e3
Cleber Simm <clebersimm@gmail.com>
2024-10-06 17:58:32 -0300

    first commit

---
+++ src/main/java/br/clebersimm/projetoteste/ProjetotesteApplication.java
@@ -0,0 +1,17 @@
+package br.clebersimm.projetoteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Projeto teste")})
+@SpringBootApplication
+public class ProjetotesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetotesteApplication.class, args);
+	}
+
+}
